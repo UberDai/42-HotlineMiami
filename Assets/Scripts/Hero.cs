19d61dea9961bda7d8f0fe1a5ea88e87405cf4d4
UberDai <arthimaurer@gmail.com>
2015-06-20 03:32:21 +0200

    Init

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hero : Human
+{
+	void	Update()
+	{
+		base.Update();
+		HandleInputs();
+	}
+
+	void	HandleInputs()
+	{
+		moveDirection = Vector2.zero;
+
+		if (Input.GetButton("Right"))
+			moveDirection.x += 1f;
+		if (Input.GetButton("Left"))
+			moveDirection.x -= 1f;
+
+		if (Input.GetButton("Up"))
+			moveDirection.y += 1f;
+		if (Input.GetButton("Down"))
+			moveDirection.y -= 1f;
+
+		moveDirection = Vector3.Normalize(moveDirection);
+	}
+}
---
+++ Assets/Scripts/Human.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Human : MonoBehaviour
+{
+	public int		maxLife;
+	public float	speed;
+	[HideInInspector]
+	public Vector2	moveDirection;
+	[HideInInspector]
+	public Vector2	aimDirection;
+	public float	rotationSpeed;
+
+	public void	Update()
+	{
+		Move();
+		Rotate();
+	}
+
+	public void	Move()
+	{
+		transform.Translate(moveDirection * speed / (1 / Time.deltaTime));
+	}
+
+	void		Rotate()
+	{
+		float		angle;
+		Vector2		mousePosition;
+		Quaternion	newRotation;
+
+		mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		newRotation = Quaternion.LookRotation(transform.position - (Vector3) mousePosition, Vector3.forward);
+	    newRotation.x = 0;
+	    newRotation.y = 0;
+
+	    if (rotationSpeed > 0)
+		    transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * speed);
+	    else
+		    transform.rotation = newRotation;
+	}
+}
