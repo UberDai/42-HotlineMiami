9f48cbb62005c87d3d37fd9ab63c82577fb44b29
nsierra- <nsierra-@student.42.fr>
2015-06-20 05:44:24 +0200

    Main Camera prefab

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCamera : MonoBehaviour {
+
+	public GameObject	hero;
+
+	float				originalZ;
+
+	void Start()
+	{
+		originalZ = transform.position.z;
+
+		hero = GameObject.FindGameObjectsWithTag("Hero")[0];
+	}
+
+	void Update()
+	{
+		if (hero)
+			transform.position = new Vector3(HeroPos().x, HeroPos().y, originalZ);
+	}
+
+	Vector3	HeroPos() { return hero.transform.position; }
+}
