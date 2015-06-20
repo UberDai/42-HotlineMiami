da693cb6dd91fab71521c17edc2161fa663caec1
nsierra- <nsierra-@student.42.fr>
2015-06-20 05:10:00 +0200

    menu v1

---
+++ Assets/Scripts/Menus/MenuManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MenuManager : MonoBehaviour
+{
+	public List<GameObject>		elements = new List<GameObject>();
+	public GameObject			resumeButton;
+
+	public int rotateRange;
+
+	void Start()
+	{
+		if (resumeButton != null)
+		{
+			ResumeButton rb = resumeButton.GetComponent<ResumeButton>();
+			rb.OnResume += HidePauseMenu;
+		}
+	}
+
+	void Update()
+	{
+		foreach (GameObject e in elements)
+		{
+			e.transform.Rotate(Vector3.right * Time.deltaTime);
+		}
+	}
+
+	void HidePauseMenu()
+	{
+		transform.parent.gameObject.SetActive(false);
+	}
+}
---
+++ Assets/Scripts/Menus/ResumeButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResumeButton : MonoBehaviour
+{
+	public delegate				void ResumeEvent();
+	public event ResumeEvent	OnResume;
+
+	void OnMouseDown()
+	{
+		gameObject.SetActive(false);
+		OnResume();
+	}
+}
